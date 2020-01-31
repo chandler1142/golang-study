63b12c48dd928e777363d4c87a2707b8ed376fad
thzw1142 <thzw1142@163.com>
2020-01-31 21:10:30 +0800

    send text by bufio

--- netty/client.go
+++ netty/client.go
@@ -12,13 +12,18 @@ func main() {
 	fmt.Println("start client...")
 
 	conn, _ := net.Dial("tcp", "127.0.0.1:8090")
+
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(conn)
+
 	for {
 		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Text to send: ")
 		text, _ := reader.ReadString('\n')
 		// send to socket
-		fmt.Fprintf(conn, text + "\n")
+		//fmt.Fprintf(conn, text + "\n")
+		writer.WriteString(text)
+		writer.Flush()
 		// listen for reply
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 		fmt.Print("Message from server: "+message + " \n")
